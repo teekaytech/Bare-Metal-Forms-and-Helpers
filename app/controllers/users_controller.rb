7490ec77a80537bf586ddae280a1cae235d4444f
ebeagusamuel <samuelebeagu@gmail.com>
2020-06-24 11:00:52 +0100

    add edit user feature to the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -10,6 +14,19 @@ class UsersController < ApplicationController
       redirect_to new_users_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
     end
   end
 
